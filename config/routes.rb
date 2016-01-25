6777e7911397fa5cb8b6c766456c6c90ac803495
Christine <christinebeaubrun@gmail.com>
2016-01-24 17:02:59 -0800

    Finish toy app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
+  # users is the controller && index is the action method
+  root 'users#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
